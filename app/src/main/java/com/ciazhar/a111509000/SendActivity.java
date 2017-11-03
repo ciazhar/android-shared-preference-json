e1c77c35bbfcd8945b29c9abe4aa5d3c0196f0ab
ciazhar <daisetsunakama@gmail.com>
2017-11-03 07:44:05 +0700

    change layout and intent

--- app/src/main/java/com/ciazhar/a111509000/SendActivity.java
+++ app/src/main/java/com/ciazhar/a111509000/SendActivity.java
@@ -10,12 +10,15 @@ import android.widget.EditText;
 import com.ciazhar.a111509000.model.Message;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class SendActivity extends AppCompatActivity {
 
     EditText name, content;
     static String MY_PREF="ciazhar";
+    static List<Message> messageList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +34,18 @@ public class SendActivity extends AppCompatActivity {
         String sContent = content.getText().toString();
 
         Message message = new Message(sName,sContent,new Date(),R.drawable.chrysanthemum);
-        MainActivity.messageList.add(message);
+        messageList.add(message);
 
         Gson gson = new Gson();
-        String jsonMessage = gson.toJson(MainActivity.messageList);
+        String jsonMessage = gson.toJson(messageList);
 
         SharedPreferences.Editor editor = getSharedPreferences(MY_PREF,MODE_PRIVATE).edit();
         editor.putString("allMessage",jsonMessage);
         editor.apply();
 
         Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
